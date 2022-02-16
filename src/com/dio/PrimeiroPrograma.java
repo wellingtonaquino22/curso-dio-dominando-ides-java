41c3bceb145f04ceb9d6c7d8d1f7b27656d97af7
Wellington Aquino <wel201617@gmail.com>
2022-02-16 11:02:26 -0300

    First Commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		System.out.println();
+		
+	}
+
+}
---
+++ src/com/dio/model/Gato.java
@@ -0,0 +1,18 @@
+package com.dio.model;
+
+public class Gato {
+	
+	String nome;
+	String cor;
+	int idade;
+	
+	public Gato( String nome, String cor, int idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+		
+		
+		
+	}
+
+}
